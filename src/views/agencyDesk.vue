02686fb4e91988859b27d9fbbdf4b4b2eccfeca8
lejnd <545404647@qq.com>
2019-01-23 19:45:57 +0800

    coding

--- src/views/agencyDesk.vue
+++ src/views/agencyDesk.vue
@@ -12,7 +12,36 @@
     </div>
     <search-bar title="违规旅行社重点监管名单" @getQuery="getList"></search-bar>
     <div class="list-box">
-
+        <div class="right">
+            <Table
+            highlight-row
+            stripe
+            :columns="columns"
+            :data="tableData"
+            size="small">
+            </Table>
+        </div>
+        <div class="left">
+            <div class="msg">
+                <Icon type="md-volume-up" :color="iconColor" />{{msg.name}}新增一家扣分旅行社
+            </div>
+            <div class="roll-header">
+                <ul class="table-row">
+                    <li v-for="name in names" :key="name">{{name}}</li>
+                </ul>
+            </div>
+            <div class="roll-body">
+                <div class="roll" :class="{anim: animate}" @mouseenter="stopRoll" @mouseleave="roll">
+                    <ul class="table-row" v-for="(item, index) in rollData" :key="index">
+                        <li><Tooltip :content="item.name">{{item.name}}</Tooltip></li>
+                        <li><Tooltip :content="item.department">{{item.department}}</Tooltip></li>
+                        <li><Tooltip :content="item.reason">{{item.reason}}</Tooltip></li>
+                        <li><Tooltip :content="item.while">{{item.while}}</Tooltip></li>
+                        <li><Tooltip :content="item.deduction">{{item.deduction}}</Tooltip></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 </template>
@@ -27,6 +56,62 @@ export default {
     data() {
         return {
             myChart: null,
+            msg: { name: '大理' },
+            iconColor: '',
+            animate: false,
+            interval: null,
+            columns: [{
+                align: 'center',
+                key: 'name',
+                title: '旅行社名称',
+                tooltip: true
+            }, {
+                align: 'center',
+                key: 'deduction',
+                title: '累计扣分',
+                tooltip: true
+            }, {
+                align: 'center',
+                key: 'department',
+                title: '扣分部门',
+                tooltip: true
+            }, {
+                align: 'center',
+                key: 'area',
+                title: '所属区域',
+                tooltip: true
+            }],
+            names: [ '旅行社名称', '扣分部门', '扣分原因', '累计时间', '累计扣分' ],
+            tableData: [
+                { name: '昆明A旅行社', deduction: '1', department: '昆明旅发委', area: '昆明市五华区' },
+                { name: '昆明A旅行社', deduction: '2', department: '昆明旅发委', area: '昆明市五华区' },
+                { name: '昆明A旅行社', deduction: '3', department: '昆明旅发委', area: '昆明市五华区' },
+                { name: '昆明A旅行社', deduction: '4', department: '昆明旅发委', area: '昆明市五华区' },
+                { name: '昆明A旅行社', deduction: '5', department: '昆明旅发委', area: '昆明市五华区' },
+                { name: '昆明A旅行社', deduction: '6', department: '昆明旅发委', area: '昆明市五华区' },
+                { name: '昆明A旅行社', deduction: '7', department: '昆明旅发委', area: '昆明市五华区' },
+                { name: '昆明A旅行社', deduction: '8', department: '昆明旅发委', area: '昆明市五华区' },
+                { name: '昆明A旅行社', deduction: '9', department: '昆明旅发委', area: '昆明市五华区' },
+                { name: '昆明A旅行社', deduction: '10', department: '昆明旅发委', area: '昆明市五华区' },
+                { name: '昆明A旅行社', deduction: '11', department: '昆明旅发委', area: '昆明市五华区' },
+                { name: '昆明A旅行社', deduction: '12', department: '昆明旅发委', area: '昆明市五华区' },
+                { name: '昆明A旅行社', deduction: '13', department: '昆明旅发委', area: '昆明市五华区' },
+            ],
+            rollData: [
+                { name: '昆明A旅行社', deduction: '1', department: '昆明旅发委', reason: '昆明市五华区', while: '2018-01-01 09:56' },
+                { name: '昆明A旅行社', deduction: '2', department: '昆明旅发委', reason: '昆明市五华区', while: '2018-01-01 09:56' },
+                { name: '昆明A旅行社', deduction: '3', department: '昆明旅发委', reason: '昆明市五华区', while: '2018-01-01 09:56' },
+                { name: '昆明A旅行社', deduction: '4', department: '昆明旅发委', reason: '昆明市五华区', while: '2018-01-01 09:56' },
+                { name: '昆明A旅行社', deduction: '5', department: '昆明旅发委', reason: '昆明市五华区', while: '2018-01-01 09:56' },
+                { name: '昆明A旅行社', deduction: '6', department: '昆明旅发委', reason: '昆明市五华区', while: '2018-01-01 09:56' },
+                { name: '昆明A旅行社', deduction: '7', department: '昆明旅发委', reason: '昆明市五华区', while: '2018-01-01 09:56' },
+                { name: '昆明A旅行社', deduction: '8', department: '昆明旅发委', reason: '昆明市五华区', while: '2018-01-01 09:56' },
+                { name: '昆明A旅行社', deduction: '9', department: '昆明旅发委', reason: '昆明市五华区', while: '2018-01-01 09:56' },
+                { name: '昆明A旅行社', deduction: '10', department: '昆明旅发委', reason: '昆明市五华区', while: '2018-01-01 09:56' },
+                { name: '昆明A旅行社', deduction: '11', department: '昆明旅发委', reason: '昆明市五华区', while: '2018-01-01 09:56' },
+                { name: '昆明A旅行社', deduction: '12', department: '昆明旅发委', reason: '昆明市五华区', while: '2018-01-01 09:56' },
+                { name: '昆明A旅行社', deduction: '13', department: '昆明旅发委', reason: '昆明市五华区', while: '2018-01-01 09:56' },
+            ],
         }
     },
     methods: {
@@ -59,10 +144,22 @@ export default {
             this.myChart = echarts.init(dom)
             this.myChart.setOption(option)
         },
+        roll() {
+            this.interval = setInterval(() => {
+                this.animate = true;
+                setTimeout(() => {
+                    this.rollData.push(this.rollData[0]);
+                    this.rollData.shift();
+                    this.animate = false;
+                }, 500)
+            }, 2000)
+        },
+        stopRoll() {
+            clearInterval(this.interval);
+        }
     },
-    mounted () {
-        this.initMap()
-        this.initRateBar()
+    mounted() {
+        this.roll()
     }
 }
 </script>
@@ -98,6 +195,69 @@ export default {
         margin: 0 20px;
         margin-bottom: 40px;
         min-height: 200px;
+        display: flex;
+        justify-content: space-between;
+        .right {
+            width: 49%;
+            td {
+                height: 30px;
+            }
+            .ivu-table-cell {
+                padding: 0;
+            }
+        }
+        .left {
+            width: 49%;
+            margin: 0;
+            padding: 0;
+            li {
+                list-style: none;
+            }
+            .msg {
+                height: 30px;
+                line-height: 30px;
+                white-space: nowrap;
+                overflow: hidden;
+                background-color: antiquewhite;
+                padding-left: 5px;
+                i {
+                    font-size: 16px;
+                    padding: 0 5px;
+                    vertical-align: sub;
+                    color: coral;
+                }
+            }
+            ul.table-row {
+                display: flex;
+                width: 100%;
+                height: 30px;
+                align-items: center;
+                li {
+                    flex: 1;
+                    text-align: center;
+                    white-space: nowrap;
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                    &:last-child {
+                        flex: none;
+                        width: 58px;
+                        padding-right: 10px;
+                    }
+                }
+            }
+            .roll-header {
+                font-weight: 600;
+                background-color: #f3f3f3;
+            }
+            .roll-body {
+                height: 300px;
+                overflow: hidden;
+                .anim{
+                    transition: all 0.5s;
+                    margin-top: -30px;
+                }
+            }
+        }
     }
 }
     
